3342a746b8dd11692693da1947fbeb4885d964c2
Ryan France <ryanfrance@digitalnerdsstudios.org>
2015-04-14 04:15:37 +0100

    Changes for now
    However it is a bit basic, il try to make things a bit more complex
    over the next few hours.

--- scripts/output.js
+++ scripts/output.js
@@ -1,4 +1,4 @@
-//<!DOCTYPE html> needs to be output manually 
+//<!DOCTYPE html> needs to be output manually
 //This way will exclude any comments that are not a child of the HTML node
 
 function grabCode() {
@@ -8,10 +8,10 @@ function grabCode() {
 
 //jQuery below line
 
-$(function () {
+$(function() {
 
 	//Will look for editor class (ace editor)
-	function parseHtml() {
+	/*function parseHtml() {
 		var html = $('.editor').val();
 		return html;
 	}
@@ -21,4 +21,45 @@ $(function () {
 		return css;
 	}
 
-});
+	$('.live').live("keyup",function() {
+		var parse = $('#output-window')[0].contentWindow.document;
+		parse.open();
+		parse.close();
+
+
+		var html = parseHtml();
+		var css = parseCSS();
+
+		$('body',parse).append(html);
+		$('head',parse).append('<style>' + css + '</style>');
+	});
+
+	$('#saveCode').click(function() {
+		$('#output-window').contents().find('html').html();
+		alert
+		return false;
+	})*/
+
+	$('.grid').height( $(window).height() );
+
+	var liveWindow 	= $('iframe'),
+		contents 	= liveWindow.contents(),
+		body 		= contents.find('body'),
+		style 		= contents.find('head').append('<style></style>').children('style');
+
+	$('.ace').focus(function(){
+
+		var $this = $(this);
+		$this.keyup(function(){
+
+			if( $this.attr('id') === 'editor' ) {
+				body.html( $this.val() );
+			}
+			else {
+				style.text( $this.val() );
+			}
+
+		});
+	});
+
+})();
